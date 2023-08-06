c779138cec681bc8567ae808106b7a9a1b7d0dfa
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-08-06 16:05:47 +0200

    Auto format lua files in helix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require 'wezterm'
 
 return {
-    font = wezterm.font("JetBrains Mono"),
-    font_size = 16,
-    color_scheme = "OneDark (base16)",
-    hide_tab_bar_if_only_one_tab = true,
-    -- Maximize the window on start-up
-    initial_cols = 999,
-    initial_rows = 999,
-    audible_bell = "Disabled",
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 16,
+  color_scheme = "OneDark (base16)",
+  hide_tab_bar_if_only_one_tab = true,
+  -- Maximize the window on start-up
+  initial_cols = 999,
+  initial_rows = 999,
+  audible_bell = "Disabled",
 }
