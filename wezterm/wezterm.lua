3c3bd88c9666925c0e3ec63dd7b0992bf5786722
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-08-07 10:52:11 +0200

    Change theme to Catpuccin Macchiato

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font("JetBrains Mono"),
   font_size = 16,
-  color_scheme = "OneDark (base16)",
+  color_scheme = "Catppuccin Macchiato",
   hide_tab_bar_if_only_one_tab = true,
   -- Maximize the window on start-up
   initial_cols = 999,
