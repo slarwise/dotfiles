52af12537d043cee92b903cb1a4f47748576b8a5
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-08-19 14:32:53 +0200

    Do not maximize wezterm on startup when using wm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,9 +5,6 @@ return {
   font_size = 16,
   color_scheme = "Sonokai (Gogh)",
   hide_tab_bar_if_only_one_tab = true,
-  -- Maximize the window on start-up
-  initial_cols = 999,
-  initial_rows = 999,
   audible_bell = "Disabled",
   window_background_opacity = 0.95,
   window_decorations = "RESIZE",
