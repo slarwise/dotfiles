badaacc126913a4d714f4a8fce136977f48b848e
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-09-13 22:59:10 +0200

    Don't adjust wezterm window size when changing font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,4 +9,5 @@ return {
   window_background_opacity = 0.95,
   window_decorations = "RESIZE",
   send_composed_key_when_right_alt_is_pressed = false,
+  adjust_window_size_when_changing_font_size = false,
 }
